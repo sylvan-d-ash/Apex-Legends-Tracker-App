262f375281423cbb6a6ef1828282887fdee53cdb
Sylvan Ash <sylvan.d.ash@gmail.com>
2019-09-24 15:30:17 +0300

    start page design

---
+++ Apex Legends Tracker/AppDelegate/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Apex Legends Tracker
+//
+//  Created by Sylvan Ash on 24/09/2019.
+//  Copyright © 2019 Sylvan Ash. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
---
+++ Apex Legends Tracker/Start/StartViewController.swift
@@ -0,0 +1,14 @@
+//
+//  StartViewController.swift
+//  Apex Legends Tracker
+//
+//  Created by Sylvan Ash on 24/09/2019.
+//  Copyright © 2019 Sylvan Ash. All rights reserved.
+//
+
+import Foundation
+
+
+class StartViewController: UIViewController {
+    //
+}
